5e41de1b99fe6e6aa45f47543d01d6141246c662
David Hyman <dghyman@gmail.com>
2014-10-19 11:55:41 -0400

    added tests

--- ViewModelTestTests/ViewModelTestTests.m
+++ ViewModelTestTests/ViewModelTestTests.m
@@ -8,6 +8,17 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "VMViewViewModel.h"
+#import "VMViewWithBlock.h"
+#import "VMViewWithDelegate.h"
+
+@interface VMViewWithBlock()
+@property (nonatomic, strong) UILabel *label;
+@end
+
+@interface VMViewWithDelegate()
+@property (nonatomic, strong) UILabel *label;
+@end
 
 @interface ViewModelTestTests : XCTestCase
 
@@ -25,16 +36,111 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void)test_01_blockImplementation_initialSet {
+    VMViewViewModel *model = [[VMViewViewModel alloc] init];
+    model.labelText = @"whatever";
+    
+    VMViewWithBlock *vmView = [[VMViewWithBlock alloc] init];
+    [vmView setViewModel:model];
+    
+    XCTAssertNotNil(model.labelText);
+    XCTAssertNotNil(vmView.label.text);
+    XCTAssertEqualObjects(model.labelText, vmView.label.text);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)test_02_blockImplementation_initialSet_thenUpdate {
+    VMViewViewModel *model = [[VMViewViewModel alloc] init];
+    model.labelText = @"whatever";
+    
+    VMViewWithBlock *vmView = [[VMViewWithBlock alloc] init];
+    [vmView setViewModel:model];
+    
+    XCTAssertNotNil(model.labelText);
+    XCTAssertNotNil(vmView.label.text);
+    
+    XCTAssertEqualObjects(model.labelText, vmView.label.text);
+    
+    model.labelText = @"now what?";
+    XCTAssertEqualObjects(model.labelText, vmView.label.text);
+}
+
+- (void)test_03_delegateImplementation_initialSet {
+    VMViewViewModel *model = [[VMViewViewModel alloc] init];
+    model.labelText = @"whatever";
+    
+    VMViewWithDelegate *vmView = [[VMViewWithDelegate alloc] init];
+    model.updateDelegate = vmView;
+    
+    XCTAssertNotNil(model.labelText);
+    XCTAssertNotNil(vmView.label.text);
+    XCTAssertEqualObjects(model.labelText, vmView.label.text);
+}
+
+- (void)test_04_delegateImplementation_initialSet_thenUpdate {
+    VMViewViewModel *model = [[VMViewViewModel alloc] init];
+    model.labelText = @"whatever";
+    
+    VMViewWithDelegate *vmView = [[VMViewWithDelegate alloc] init];
+    model.updateDelegate = vmView;
+    
+    XCTAssertNotNil(model.labelText);
+    XCTAssertNotNil(vmView.label.text);
+    XCTAssertEqualObjects(model.labelText, vmView.label.text);
+
+    model.labelText = @"now what?";
+    XCTAssertEqualObjects(model.labelText, vmView.label.text);
+}
+
+- (void)test_05_batchUpdates {
+    VMViewViewModel *model = [[VMViewViewModel alloc] init];
+    model.labelText = @"whatever";
+    
+    VMViewWithDelegate *vmView = [[VMViewWithDelegate alloc] init];
+    model.updateDelegate = vmView;
+    
+    XCTAssertNotNil(model.labelText);
+    XCTAssertNotNil(vmView.label.text);
+    XCTAssertEqualObjects(model.labelText, vmView.label.text);
+
+    // batch updates
+    // doesn't actually update til the end of the block
+    // normally you would update mutliple model properties here
+    // and the view would only update once
+    // instead of every time you update a model property
+    // this is a bs way of testing it
+    // but it's accurate
+    [model performBatchUpdatesWithBlock:^{
+        model.labelText = @"1";
+        XCTAssertNotEqualObjects(model.labelText, vmView.label.text);
+        model.labelText = @"2";
+        XCTAssertNotEqualObjects(model.labelText, vmView.label.text);
+        model.labelText = @"3";
+        XCTAssertNotEqualObjects(model.labelText, vmView.label.text);
+        model.labelText = @"4";
     }];
+    XCTAssertEqualObjects(model.labelText, vmView.label.text);
+}
+
+- (void)test_06_changeView {
+    VMViewViewModel *model = [[VMViewViewModel alloc] init];
+    model.labelText = @"whatever";
+    
+    VMViewWithDelegate *vmView1 = [[VMViewWithDelegate alloc] init];
+    model.updateDelegate = vmView1;
+    
+    XCTAssertNotNil(model.labelText);
+    XCTAssertNotNil(vmView1.label.text);
+    XCTAssertEqualObjects(model.labelText, vmView1.label.text);
+
+    VMViewWithDelegate *vmView2 = [[VMViewWithDelegate alloc] init];
+    model.updateDelegate = vmView2;
+    
+    XCTAssertEqualObjects(vmView1.label.text, vmView2.label.text);
+
+    model.labelText = @"now what?";
+    
+    XCTAssertNotEqualObjects(model.labelText, vmView1.label.text);
+    XCTAssertEqualObjects(model.labelText, vmView2.label.text);
 }
 
 @end
